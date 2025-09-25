c41dc2f8891b3c0839a2d5765ec28a86d120f5e1
CiscG <fgss98@gmail.com>
2025-09-25 20:31:13 -0300

    teste api

--- SIMFPP/BackEnd/server.js
+++ SIMFPP/BackEnd/server.js
@@ -14,13 +14,15 @@ const database = new DatabaseMemory()
 //Route Parameter
 
 
-server.post('/videos', () =>{
+server.post('/videos', (request, reply) =>{
    database.create({
       title: 'Video 01',
       description: 'Esse é o video 01',
       duration: 180,
    })
    console.log(database.list())
+   
+   return reply.status(201).send()
 })
 
 server.get('/videos', () =>{
