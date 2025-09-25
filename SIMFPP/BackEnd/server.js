eb30aa979796b22835eb0bb33fb2d1b99116b5dd
CiscG <fgss98@gmail.com>
2025-09-25 20:16:12 -0300

    insert banco teste

--- SIMFPP/BackEnd/server.js
+++ SIMFPP/BackEnd/server.js
@@ -1,6 +1,38 @@
 import { fastify } from 'fastify'
+import { DatabaseMemory } from './databese-memory.js'
+
 
 const server = fastify()
+
+const database = new DatabaseMemory()
+
+//GET, busco informação
+//POST, criação https://localhost:3333/videos
+//PUT, alteração https://localhost:3333/video/3
+//DELETE, https://localhost:3333/video/3
+//PATCH, altera pequenos ddados
+//Route Parameter
+
+
+server.post('/videos', () =>{
+   database.create({
+      title: 'Video 01',
+      description: 'Esse é o video 01',
+      duration: 180,
+   })
+   console.log(database.list())
+})
+
+server.get('/videos', () =>{
+   return 'Hello Rocketseat'
+})
+
+server.put('/videos/:id', () => {
+   return 'Hello Node.js'
+})
+server.delete('/videos/:id', () => {
+   return 'Hello Node.js'
+})
 
 server.get('/', () => {
    return 'Hello World'
